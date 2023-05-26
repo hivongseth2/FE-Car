5a3036a227858b7dfaae67fdd053a09e72079961
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2023-05-26 11:46:18 +0700

    a

---
+++ src/views/Menu.js
@@ -0,0 +1,26 @@
+import "../styles/Menu.scss"
+import { Link} from "react-router-dom";
+const Menu = () => {
+    return (
+        <div className="main">
+            <div className="navbar">
+                <h2 className="logo">WEBNAME</h2>
+            </div>
+            <div className="menu">
+                <ul>
+                    <li><Link to="/">TRANG CHỦ</Link></li>
+                    <li><a href="/#">GIỚI THIỆU</a></li>
+                    <li><a href="/#">LUYỆN THI</a></li>
+                    <li><a href="/#">BÀI HỌC</a></li>   
+                </ul>
+            </div>
+            <div className="login-register">
+                <ul>
+                    <li><Link to="/register">ĐĂNG KÝ</Link></li>
+                    <li><Link to="/login">ĐĂNG NHẬP</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+export default Menu;
