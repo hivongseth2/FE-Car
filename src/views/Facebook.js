0cff3b140e55c39602b2caf1f262ef647bb0131a
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2023-06-02 23:11:01 +0700

    social

--- src/views/Facebook.js
+++ src/views/Facebook.js
@@ -9,47 +9,55 @@ const Facebook = () => {
 
   return (
     <>
-      <div
-        class="fb-page"
-        data-href="https://www.facebook.com/profile.php?id=100078023214277"
-        data-tabs="timeline"
-        data-width=""
-        data-height=""
-        data-small-header="false"
-        data-adapt-container-width="true"
-        data-hide-cover="false"
-        data-show-facepile="true"
-      >
-        <blockquote
-          cite="https://www.facebook.com/profile.php?id=100078023214277"
-          class="fb-xfbml-parse-ignore"
+      <div className="facebook">
+        <div
+          class="fb-page"
+          data-href="https://www.facebook.com/profile.php?id=100078023214277"
+          data-tabs="timeline"
+          data-width=""
+          data-height=""
+          data-small-header="false"
+          data-adapt-container-width="true"
+          data-hide-cover="false"
+          data-show-facepile="true"
         >
-          <a href="https://www.facebook.com/profile.php?id=100078023214277">
-            Kaha Café.
-          </a>
-        </blockquote>
-      </div>
-      <blockquote
-        class="tiktok-embed"
-        cite="https://www.tiktok.com/@thanhluan1303"
-        data-unique-id="thanhluan1303"
-        data-embed-type="creator"
-      >
-        <section>
-          {" "}
-          <a
-            target="_blank"
-            href="https://www.tiktok.com/@thanhluan1303?refer=creator_embed"
+          <blockquote
+            cite="https://www.facebook.com/profile.php?id=100078023214277"
+            class="fb-xfbml-parse-ignore"
           >
-            @thanhluan1303
-          </a>{" "}
-        </section>{" "}
-      </blockquote>{" "}
-      <iframe
-        width="420"
-        height="345"
-        src="https://www.youtube.com/embed/XGSy3_Czz8k"
-      ></iframe>
+            <a href="https://www.facebook.com/profile.php?id=100078023214277">
+              Kaha Café.
+            </a>
+          </blockquote>
+        </div>
+      </div>
+
+      <div className="tiktok">
+        <blockquote
+          class="tiktok-embed"
+          cite="https://www.tiktok.com/@thanhluan1303"
+          data-unique-id="thanhluan1303"
+          data-embed-type="creator"
+        >
+          <section>
+            {" "}
+            <a
+              target="_blank"
+              href="https://www.tiktok.com/@thanhluan1303?refer=creator_embed"
+            >
+              @thanhluan1303
+            </a>{" "}
+          </section>{" "}
+        </blockquote>{" "}
+      </div>
+
+      <div className="youtube">
+        <iframe
+          width="420"
+          height="345"
+          src="https://www.youtube.com/embed/XGSy3_Czz8k"
+        ></iframe>
+      </div>
     </>
   );
 };
