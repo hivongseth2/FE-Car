7e4f424489ef7e3cbbb7228f5a8ae3f7a0bc51d1
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2023-06-09 14:59:03 +0700

    push doi mat khau

--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,17 +1,31 @@
-// import "../styles/Nav.scss";
-import {NavLink } from "react-router-dom";
-import "../styles/Nav.scss";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
-import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
-import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt,
+  faShareAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import logo from "../img/Logo-IUH.jpg";
+import "../styles/Nav.scss";
+import FlastInfo from "./Forms/FlastInfo";
+
 const Nav = () => {
+  const token = localStorage.getItem("token");
+  const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className="topnav">
       <div className="logo">
-        <img src={logo} alt="logo" className="logo"></img>
+        <img src={logo} alt="logo" className="logo" />
       </div>
       <div className="mainMenu">
         <NavLink to="/">
@@ -19,27 +33,31 @@ const Nav = () => {
           Trang chủ
         </NavLink>
         <NavLink to="/socialmedia">
-          {" "}
           <FontAwesomeIcon icon={faShareAlt} />
           Social Media
         </NavLink>
       </div>
       <NavLink to="/edit-admin">Chỉnh sửa thông tin</NavLink>
       <NavLink to="/info-sudent">Thông tin học viên</NavLink>
-      <div className="submenu">
-
-        <NavLink to="/login">
-          <FontAwesomeIcon icon={faSignInAlt} />
-          Đăng Nhập
-        </NavLink>
-        <NavLink to="/register">
-          <FontAwesomeIcon icon={faUserPlus} />
-          Đăng kí
-        </NavLink>
-        <span className="hotline">Holine:0909789789</span>
-      </div>
-      {/* <NavLink to="/Cart">Giỏ Hàng </NavLink> */}
+      {isLoggedIn ? (
+        <div className="submenu">
+          <FlastInfo onLogout={handleLogout} />
+        </div>
+      ) : (
+        <div className="submenu">
+          <NavLink to="/login">
+            <FontAwesomeIcon icon={faSignInAlt} />
+            Đăng Nhập
+          </NavLink>
+          <NavLink to="/register">
+            <FontAwesomeIcon icon={faUserPlus} />
+            Đăng kí
+          </NavLink>
+          <span className="hotline">Holine:0909789789</span>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Nav;
