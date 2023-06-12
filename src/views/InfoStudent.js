d211f22fbcefc34484ce59c70eb12eb304d5ef39
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2023-06-13 06:19:37 +0700

    pull nav

--- src/views/InfoStudent.js
+++ src/views/InfoStudent.js
@@ -80,10 +80,10 @@ const InfoStudent = () => {
   }, []);
 
   if (data.length === 0) {
-    return <div>Loading....</div>;
+    return <Loading />;
   }
   if (datafollow.length === 0) {
-    return <div>Loading follow....</div>;
+    return <Loading />;
   }
   const formattedBirthday = format(new Date(data.birthday), "dd/MM/yyyy");
 
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faHome,
+  faGlobe,
   faBars,
   faUserPlus,
   faSignInAlt,
-  faSignOutAlt,
-  faShareAlt,
+  faCircleInfo,
+  faPenToSquare,
+  faHouse,
 } from "@fortawesome/free-solid-svg-icons";
-import logo from "../img/Logo-IUH.jpg";
+import logo from "../img/logott.jpg";
 import "../styles/Nav.scss";
 import FlastInfo from "./Forms/FlastInfo";
 
@@ -24,38 +25,49 @@ const Nav = () => {
   };
   return (
     <div className="topnav">
-      {/* <div className="logo">
-          <img src={logo} alt="logo" className="logo" />
-        </div> */}
       <input type="checkbox" id="menu-toggle" className="menu-toggle" />
       <label htmlFor="menu-toggle" className="menu-icon">
-        <FontAwesomeIcon icon={faBars} />
+      <FontAwesomeIcon icon={faBars} size="xs" style={{color: "#000000",}} />
       </label>
       <div className="mainMenu">
+        <NavLink to="/" exact activeClassName="custom-active">
+          <div className="logo-img">
+            <img src={logo} alt="logo" className="logo" />
+          </div>
+        </NavLink>
         <NavLink to="/" exact={true}>
-          <FontAwesomeIcon icon={faHome} />
+          {/* <FontAwesomeIcon icon={faHouse} size="2xs" style={{ color: "#000000" }} /> {""} */}
           Trang chủ
         </NavLink>
 
         <NavLink to="/page-mxh">
-          <FontAwesomeIcon icon={faShareAlt} />
+          {/* <FontAwesomeIcon icon={faGlobe} size="2xs" style={{ color: "#000000" }} /> {""} */}
           Mạng xã hội
         </NavLink>
-        <NavLink to="/admin-login">Chỉnh sửa thông tin</NavLink>
-        <NavLink to="/info-sudent">Thông tin học viên</NavLink>
+        <NavLink to="/admin-login">
+          {/* <FontAwesomeIcon icon={faPenToSquare} size="2xs" style={{ color: "#000000" }} />{" "} */}
+          {""}
+          Chỉnh sửa thông tin
+        </NavLink>
+        <NavLink to="/info-sudent">
+          {/* <FontAwesomeIcon icon={faCircleInfo} size="2xs" style={{ color: "#000000" }} />{" "} */}
+          {""}
+          Thông tin học viên
+        </NavLink>
       </div>
       {isLoggedIn ? (
         <div className="submenu fast-info">
-        <FlastInfo onLogout={handleLogout} />
-      </div>
+          <FlastInfo onLogout={handleLogout} />
+        </div>
+
       ) : (
         <div className="submenu">
           <NavLink to="/login">
-            <FontAwesomeIcon icon={faSignInAlt} />
+            {/* <FontAwesomeIcon size="2xs" icon={faSignInAlt} /> {" "} */}
             Đăng Nhập
           </NavLink>
           <NavLink to="/register">
-            <FontAwesomeIcon icon={faUserPlus} />
+            {/* <FontAwesomeIcon size="2xs" icon={faUserPlus} /> {" "}  */}
             Đăng kí
           </NavLink>
           <span className="hotline">Hotline: 0909789789</span>
