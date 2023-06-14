c4bc82df296c5558987509392c18c708cb3c24be
hivongseth2 <99702632+hivongseth2@users.noreply.github.com>
2023-06-14 11:20:28 +0700

    done NAV

--- src/views/Nav1.js
+++ src/views/Nav1.js
@@ -4,6 +4,7 @@ import "../styles/Nav1.scss"; // Import file CSS tương ứng
 import { NavLink } from "react-router-dom";
 import logo from "../img/zyro-image.png";
 import login from "../img/login.png";
+import menu from "../img/menu.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FlastInfo from "./Forms/FlastInfo";
 
@@ -26,6 +27,23 @@ const Nav1 = () => {
     setIsLoggedIn(false);
   };
 
+  // const isMobile = window.innerWidth < 768; // Xác định nếu là điện thoại di động
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   const [isAtTop, setIsAtTop] = useState(true);
 
   useEffect(() => {
@@ -38,35 +56,132 @@ const Nav1 = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <nav className={`navbar ${isAtTop ? "" : "transparent-bg"}`}>
-      <div className="logo">
-        <img src={logo} className="logoct" alt="logo" />
-      </div>
+    <>
+      {isMobile ? (
+        <nav className={`navbar ${isAtTop ? "" : "transparent-bg"}`}>
+          <div className="logo">
+            <img src={logo} className="logoct" alt="logo" />
+          </div>
 
-      <div id="trapezoid">
-        <a className="sub-home" href="#">
-          Home
-        </a>
+          <div id="trapezoid">
+            <a className="sub-home" href="#">
+              Home
+            </a>
+            <input
+              type="checkbox"
+              id="menu-toggle"
+              className="menu-toggle"
+              checked={isMenuOpen}
+            />
 
-        <NavLink to="/" exact={true} className="parent">
-          Trang chủ
-        </NavLink>
+            <label
+              htmlFor="menu-toggle"
+              className="menu-icon"
+              onClick={toggleMenu}
+            >
+              <img src={logo} className="menuIcon" alt="menu" />
+            </label>
 
-        <NavLink to="/page-mxh" className="parent">
-          Mạng xã hội
-        </NavLink>
+            <div className={`menu ${isMenuOpen ? "open" : ""}`}>
+              {/* Nội dung menu */}
+              <div className={`menu-content `}>
+                <NavLink
+                  to="/"
+                  exact={true}
+                  className="parent"
+                  onClick={closeMenu}
+                >
+                  Trang chủ
+                </NavLink>
 
-        {/* <NavLink to="/page-mxh" className="parent">
-          Mạng xã hội
-        </NavLink> */}
-      </div>
-      <div className="submenu">
-        <NavLink to="/login" className="subItem" style={{ border: "none" }}>
-          <img src={login} alt="logo" className="parentLogo" />
-        </NavLink>
-      </div>
-    </nav>
+                <NavLink to="/page-mxh" className="parent" onClick={closeMenu}>
+                  Mạng xã hội
+                </NavLink>
+
+                {isLoggedIn ? (
+                  <FlastInfo onLogout={handleLogout} className="rotate" />
+                ) : (
+                  <div className={` menu ${isMenuOpen ? "open" : ""}`}>
+                    <NavLink
+                      to="/login"
+                      className="subItem parent "
+                      style={{ border: "none" }}
+                    >
+                      <img src={login} alt="logo" className="parentLogo" />
+                      <span>ĐĂNG NHẬP</span>
+                    </NavLink>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </nav>
+      ) : (
+        <nav className={`navbar ${isAtTop ? "" : "transparent-bg"}`}>
+          <div className="logo">
+            <img src={logo} className="logoct" alt="logo" />
+          </div>
+
+          <div id="trapezoid">
+            <a className="sub-home" href="#">
+              Home
+            </a>
+            <input
+              type="checkbox"
+              id="menu-toggle"
+              className="menu-toggle"
+              checked={isMenuOpen}
+            />
+
+            <label
+              htmlFor="menu-toggle"
+              className="menu-icon"
+              onClick={toggleMenu}
+            >
+              <img src={logo} className="menuIcon" alt="menu" />
+            </label>
+
+            <div className={`menu ${isMenuOpen ? "open" : ""}`}>
+              {/* Nội dung menu */}
+              <div className={`menu-content `}>
+                <NavLink
+                  to="/"
+                  exact={true}
+                  className="parent"
+                  onClick={closeMenu}
+                >
+                  Trang chủ
+                </NavLink>
+
+                <NavLink to="/page-mxh" className="parent" onClick={closeMenu}>
+                  Mạng xã hội
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          {isLoggedIn ? (
+            <FlastInfo onLogout={handleLogout} className="rotate" />
+          ) : (
+            <div className={` menu ${isMenuOpen ? "open" : ""}`}>
+              <NavLink
+                to="/login"
+                className="subItem parent "
+                style={{ border: "none" }}
+              >
+                <img src={login} alt="logo" className="parentLogo" />
+              </NavLink>
+              {/* <span>ĐĂNG NHẬP</span> */}
+            </div>
+          )}
+        </nav>
+      )}{" "}
+    </>
   );
 };
 
